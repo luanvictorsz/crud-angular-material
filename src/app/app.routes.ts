74095bd31ded1fa46e0c7788610f3793b4fa5e7a
Victor <luanvitorlima2017@gmail.com>
2025-07-26 23:12:04 -0300

    criando um novo componente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Page } from '././components/page/page';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: App },
+    { path: 'page', component: Page },
+];
+
