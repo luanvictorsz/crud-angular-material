34df9d1c674abbaf7ec1efac2e960948fea9e3aa
Victor souza <luanvitorlima2017@gmail.com>
2025-08-17 17:47:55 -0300

    definindo rotas das paginas de cadastro e consulta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { PageComponent } from './components/page-component/page-component';
-import { App } from './app';
+import { Consulta } from './components/consulta/consulta';
+import { Cadastro } from './components/cadastro/cadastro';
 
 export const routes: Routes = [
-    { path: '', component: App },
+    { path: '', component: PageComponent },
     { path: 'page', component: PageComponent },
+    { path: 'consulta', component: Consulta},
+    { path: 'cadastro', component: Cadastro}
 ];
 
