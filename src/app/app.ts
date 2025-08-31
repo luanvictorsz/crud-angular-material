37605bd3833177d903ec82d83b0175342a00d8c1
Victor souza <luanvitorlima2017@gmail.com>
2025-08-31 19:41:20 -0300

    fix: refatorando e removendo itens n usados

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,20 +2,20 @@ import { Component, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { Navbar } from "./components/navbar/navbar";
 @
 Component({
   standalone: true,
   selector: 'app-root',
   imports: [
-    MatButtonModule, 
-    MatToolbarModule, 
-    RouterOutlet, 
-    RouterLink
-  ],
+    MatButtonModule,
+    MatToolbarModule,
+    Navbar
+],
 
   templateUrl: './app.html',
   styleUrls: ['./app.css'] 
 })
-export class app {
+export class AppComponent {
   protected readonly title = signal('Cadastro de clientes')
 }
