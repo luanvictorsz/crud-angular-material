cb26828ae22ab6dfa780d3aafbad4e8033a607e9
Victorsz <luanvictorlima2017@gmail.com>
2025-09-08 23:26:26 -0300

    feat: criando base do formulário

--- src/app/components/cadastro/cadastro.ts
+++ src/app/components/cadastro/cadastro.ts
@@ -1,19 +1,23 @@
 import { Component } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout'
-import { MatCardModule } from '@angular/material/card'
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
 import { Navbar } from "../navbar/navbar";
+import { InputTextModule } from "primeng/inputtext";
+import { CardModule } from "primeng/card";
+import { DividerModule } from "primeng/divider";
+import {  ButtonModule } from "primeng/button"
 
 @Component({
+  standalone: true,
   selector: 'app-cadastro',
-  standalone: true,   
   imports: [
-    FlexLayoutModule,
-    MatCardModule,
+    InputTextModule,
+    DividerModule,
+    ButtonModule,
     FormsModule,
+    CardModule,
     Navbar
   ],
   templateUrl: './cadastro.html',
-  styleUrls: ['./cadastro.css'] 
+  styleUrls: ['./cadastro.css']
 })
 export class Cadastro {}
