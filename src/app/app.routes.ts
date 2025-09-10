926d658e2f36f8691a13094604492f7cf83e316b
Victorsz <luanvictorlima2017@gmail.com>
2025-09-09 21:49:39 -0300

    atribuindo rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app';
 import { Consulta } from './components/consulta/consulta';
 import { Cadastro } from './components/cadastro/cadastro';
+import { Inicio } from './view/user/inicio/inicio';
 
 export const routes: Routes = [
     { path: '', component: AppComponent },
-    { path: 'Inicio', component: AppComponent },
+    { path: 'Inicio', component: Inicio },
     { path: 'Consulta', component: Consulta},
     { path: 'Cadastro', component: Cadastro}
 ];
